6abbdae00913230333dfb9a6e9c559012d405cd3
Cahillmn <mcahill@seattleu.edu>
2020-05-27 22:15:25 -0700

    first commit

--- src/views/Forecast.vue
+++ src/views/Forecast.vue
@@ -46,15 +46,16 @@ export default {
   created () {
     this.showLoading = true;
 
-    // TODO: Cache these API results using the City ID as the label
+    let cacheLabel = 'forecast_' + this.$route.params.cityId;
 
-    // TODO: Create a cacheLabel value
+    /*let cacheExpiry = 15 * 60 * 1000;*/
 
-    // TODO: Create a cacheExpiry value set to 15 minutes in milliseconds
-
-    // TODO: Use a conditional to check if the API query has been cached
-    // If so, use that cached data
-    // If not, make the API call and cache the data with the cacheLabel and cacheExpiry defined above
+    if (this.$ls.get(cacheLabel)) {
+      console.log('Cached value detected.');
+      this.weatherData = this.$ls.get(cacheLabel);
+      this.showLoading = false;
+    } else {
+      console.log('No cache detected. Making API request.');
 
     API.get('forecast', {
       params: {
@@ -72,6 +73,7 @@ export default {
         text: error.message
       });
     });
+    }
   },
   filters: {
     formatDate: function (timestamp){
